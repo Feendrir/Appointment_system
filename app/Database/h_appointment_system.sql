c91c830f6918a87a12cbcae9c0cdc37403f3d348
Feendrir <111201811520@mhs.dinus.ac.id>
2024-12-27 23:35:41 +0700

    Update h_appointment_system.sql

--- app/Database/h_appointment_system.sql
+++ app/Database/h_appointment_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2024 at 06:26 PM
+-- Generation Time: Dec 27, 2024 at 05:35 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,10 +30,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `daftar_poli` (
   `id` int(11) NOT NULL,
   `id_pasien` int(11) DEFAULT NULL,
+  `id_poli` int(11) NOT NULL,
   `id_jadwal` int(11) DEFAULT NULL,
   `keluhan` text DEFAULT NULL,
   `no_antrian` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `daftar_poli`
+--
+
+INSERT INTO `daftar_poli` (`id`, `id_pasien`, `id_poli`, `id_jadwal`, `keluhan`, `no_antrian`) VALUES
+(11, 1, 4, 1, 'Sakit nyeri', 1),
+(12, 1, 4, 1, 'Rasa sakit yang akut', 2),
+(13, 1, 4, 3, 'Nyeri Banget', 1);
 
 -- --------------------------------------------------------
 
@@ -46,6 +56,17 @@ CREATE TABLE `detail_periksa` (
   `id_periksa` int(11) NOT NULL,
   `id_obat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `detail_periksa`
+--
+
+INSERT INTO `detail_periksa` (`id`, `id_periksa`, `id_obat`) VALUES
+(1, 2, 7),
+(2, 2, 9),
+(3, 3, 1),
+(4, 3, 2),
+(5, 3, 4);
 
 -- --------------------------------------------------------
 
@@ -68,7 +89,7 @@ CREATE TABLE `dokter` (
 --
 
 INSERT INTO `dokter` (`id`, `nama`, `alamat`, `no_hp`, `id_poli`, `created_at`, `updated_at`) VALUES
-(1, 'Dr. Andi Rahmat', 'Jl. Merdeka No. 10, Jakarta', '081234567890', 1, '2024-12-09 17:50:21', '2024-12-09 17:50:21'),
+(1, 'Dr. Andi Rahmat', 'Jl. Merdeka No. 10', '081234567890', 4, '2024-12-09 17:50:21', '2024-12-27 16:09:17'),
 (2, 'Dr. Budi Santoso', 'Jl. Raya No. 5, Surabaya', '081234567891', 2, '2024-12-09 17:50:21', '2024-12-09 17:50:21'),
 (3, 'Dr. Citra Dewi', 'Jl. Pahlawan No. 8, Bandung', '081234567892', 3, '2024-12-09 17:50:21', '2024-12-09 17:50:21'),
 (4, 'Dr. Dedi Setiawan', 'Jl. Sudirman No. 20, Yogyakarta', '081234567893', 4, '2024-12-09 17:50:21', '2024-12-09 17:50:21'),
@@ -85,8 +106,47 @@ CREATE TABLE `jadwal_periksa` (
   `id_dokter` int(11) DEFAULT NULL,
   `hari` varchar(20) DEFAULT NULL,
   `jam_mulai` time DEFAULT NULL,
-  `jam_selesai` time DEFAULT NULL
+  `jam_selesai` time DEFAULT NULL,
+  `status` enum('aktif','nonaktif') DEFAULT 'nonaktif',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `jadwal_periksa`
+--
+
+INSERT INTO `jadwal_periksa` (`id`, `id_dokter`, `hari`, `jam_mulai`, `jam_selesai`, `status`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Kamis', '20:00:00', '21:00:00', 'nonaktif', '2024-12-25 13:34:14', '2024-12-27 23:06:40'),
+(2, 1, 'Jumat', '08:00:00', '20:00:00', 'nonaktif', '2024-12-25 13:38:25', '2024-12-27 23:06:40'),
+(3, 1, 'Rabu', '10:00:00', '16:00:00', 'nonaktif', '2024-12-25 13:39:53', '2024-12-27 23:06:40'),
+(4, 1, 'Senin', '07:00:00', '12:00:00', 'nonaktif', '2024-12-25 13:51:44', '2024-12-27 23:06:40'),
+(5, 1, 'Selasa', '10:00:00', '16:00:00', 'nonaktif', '2024-12-25 15:01:36', '2024-12-27 23:06:40'),
+(6, 3, 'Senin', '07:00:00', '10:00:00', 'aktif', '2024-12-27 22:48:40', '2024-12-27 22:48:40'),
+(7, 1, 'Sabtu', '08:00:00', '16:00:00', 'aktif', '2024-12-27 23:06:17', '2024-12-27 23:06:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `version` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `group` varchar(255) NOT NULL,
+  `namespace` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL,
+  `batch` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
+(1, '2024-12-23-161626', 'App\\Database\\Migrations\\AddStatusToJadwalPeriksa', 'default', 'App', 1734970703, 1);
 
 -- --------------------------------------------------------
 
@@ -159,6 +219,15 @@ CREATE TABLE `periksa` (
   `biaya_periksa` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `periksa`
+--
+
+INSERT INTO `periksa` (`id`, `id_daftar_poli`, `tanggal_periksa`, `catatan`, `biaya_periksa`) VALUES
+(1, 11, '2024-12-27', 'uwuw', 160000.00),
+(2, 12, '2024-12-27', 'test', 166500.00),
+(3, 13, '2024-12-27', 'Rajin Minum Obat yaa', 250000.00);
+
 -- --------------------------------------------------------
 
 --
@@ -194,7 +263,8 @@ INSERT INTO `poli` (`id`, `nama_poli`, `keterangan`, `created_at`, `updated_at`)
 ALTER TABLE `daftar_poli`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_daftar_pasien` (`id_pasien`),
-  ADD KEY `fk_daftar_jadwal` (`id_jadwal`);
+  ADD KEY `fk_daftar_jadwal` (`id_jadwal`),
+  ADD KEY `fk_id_poli` (`id_poli`);
 
 --
 -- Indexes for table `detail_periksa`
@@ -217,6 +287,12 @@ ALTER TABLE `dokter`
 ALTER TABLE `jadwal_periksa`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_jadwal_dokter` (`id_dokter`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `obat`
@@ -253,25 +329,31 @@ ALTER TABLE `poli`
 -- AUTO_INCREMENT for table `daftar_poli`
 --
 ALTER TABLE `daftar_poli`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `detail_periksa`
 --
 ALTER TABLE `detail_periksa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `dokter`
 --
 ALTER TABLE `dokter`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `jadwal_periksa`
 --
 ALTER TABLE `jadwal_periksa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `obat`
@@ -289,7 +371,7 @@ ALTER TABLE `pasien`
 -- AUTO_INCREMENT for table `periksa`
 --
 ALTER TABLE `periksa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `poli`
@@ -306,7 +388,8 @@ ALTER TABLE `poli`
 --
 ALTER TABLE `daftar_poli`
   ADD CONSTRAINT `fk_daftar_jadwal` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal_periksa` (`id`),
-  ADD CONSTRAINT `fk_daftar_pasien` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id`);
+  ADD CONSTRAINT `fk_daftar_pasien` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id`),
+  ADD CONSTRAINT `fk_id_poli` FOREIGN KEY (`id_poli`) REFERENCES `poli` (`id`);
 
 --
 -- Constraints for table `detail_periksa`
